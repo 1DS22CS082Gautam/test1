02e14af08bb9b698016b67a3fc22ce6a582e3290
1DS22CS082Gautam <136419773+1DS22CS082Gautam@users.noreply.github.com>
2023-12-13 15:12:15 +0530

    Update t1.c in dev2 branch

--- t1.c
+++ t1.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-int add(){
+void add(){
  int a,b,sum;
  sum=a+b;
  printf("sum=%d",sum);
+}
+void sub(){
+ int a,b,sub;
+ sum=a-b;
+ printf("sub=%d",sub);
 }
 int main(){
  add()
